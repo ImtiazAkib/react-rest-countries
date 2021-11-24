33198f378d7aba477b4937ce26adf865aaf61bb4
ImtiazAkib <imtiazakib007@gmail.com>
2021-11-24 19:52:35 +0600

    finish

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -16,15 +16,28 @@ const Countries = () => {
       .then((res) => res.json())
       .then((data) => setCountryflags(data.data));
   }, []);
+
+  const [countrycodes, setCountrycodes] = useState([]);
+  useEffect(() => {
+    fetch("https://countriesnow.space/api/v0.1/countries/iso")
+      .then((res) => res.json())
+      .then((data) => setCountrycodes(data.data));
+  }, []);
+
   return (
-    <div className="countries">
+    <div>
       <h1>Hello from Countries</h1>
-      {countries.map((country) => (
-        <Country name={country.name} capital={country.capital}></Country>
-      ))}
-      {countryflags.map((country) => (
-        <Country flag={country.flag}></Country>
-      ))}
+      <div className="countries">
+        {countries.map((country) => (
+          <Country name={country.name} capital={country.capital}></Country>
+        ))}
+        {countryflags.map((country) => (
+          <Country flag={country.flag}></Country>
+        ))}
+        {countrycodes.map((codes) => {
+          <Country key={codes.Iso3}></Country>;
+        })}
+      </div>
     </div>
   );
 };
